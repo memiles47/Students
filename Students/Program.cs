ac539dce2f622754f696b7ca93080bae2a6bc08d
Michael E Miles <michael@miles-me.net>
2017-05-10 16:04:18 -0500

    Initial Commit

--- Students/Program.cs
+++ Students/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Principal;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,24 @@ namespace Students
     {
         static void Main(string[] args)
         {
+            var alison = new ArrayList() {"ID0001", 26, "A"};
+            var bob = new ArrayList() {"ID0002", 27, "B"};
+            var claire = new ArrayList() {"ID0003", 17, "C"};
+            var dan = new ArrayList() {"ID0004", 21, "D"};
+            var emma = new ArrayList() {"ID0005", 22, "E"};
+
+
+            foreach (var item in alison)
+                Console.WriteLine(item);
+
+            var students = new Dictionary<string, ArrayList>
+            {
+                {"Alison", alison},
+                { "Bob", bob},
+                { "Claire", claire},
+                { "Dan", dan},
+                { "Emma", emma}
+            };
         }
     }
 }
